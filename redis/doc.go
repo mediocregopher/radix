abefa8ef1b20461cc1e7aeca0e3805ff84ad2b3c
fzzbt <juhanipm@gmail.com>
2012-01-01 19:47:33 +0200

    small godoc fix

--- redis/doc.go
+++ redis/doc.go
@@ -1,7 +1,5 @@
-/*
-	Radix -- Asynchronous Redis client for Go.
-*/
-
+//	Radix -- Asynchronous Redis client for Go.
+//
 // Simple client for accessing the Redis database. After establishing a connection
 // with NewRedisDatabase() commands can be executed with Command(). So every command
 // is possible. The method returns a ResultSet with different methods for success 
