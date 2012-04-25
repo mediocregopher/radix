7d777e27193da8736b84cc5bd3afb17d63b151c8
Bobby Powers <bobbypowers@gmail.com>
2012-04-25 09:57:52 -0400

    conn: reduce indentation in handlePublishing

--- radix/connection.go
+++ radix/connection.go
@@ -418,104 +418,105 @@ func (c *connection) handlePublishing(ed *envData) {
 		// NOTE: Redis SHOULD NOT send error replies while the connection is in pub/sub mode.
 		// These errors must always originate from radix itself.
 		c.messageChan <- &Message{Type: MessageError, Error: r.Error()}
-	} else {
-		var r0, r1 *Reply
-		m := &Message{}
-
-		if r.Type() != ReplyMulti || r.Len() < 3 {
-			goto Invalid
-		}
-
-		r0 = r.At(0)
-		if r0.Type() != ReplyString {
-			goto Invalid
-		}
-
-		// first argument is the message type
-		switch r0.Str() {
-		case "subscribe":
-			m.Type = MessageSubscribe
-		case "unsubscribe":
-			m.Type = MessageUnsubscribe
-		case "psubscribe":
-			m.Type = MessagePSubscribe
-		case "punsubscribe":
-			m.Type = MessagePUnsubscribe
-		case "message":
-			m.Type = MessageMessage
-		case "pmessage":
-			m.Type = MessagePMessage
-		default:
-			goto Invalid
-		}
-
-		// second argument
-		r1 = r.At(1)
-		if r1.Type() != ReplyString {
-			goto Invalid
-		}
-
-		switch {
-		case m.Type == MessageSubscribe || m.Type == MessageUnsubscribe:
-			m.Channel = r1.Str()
-
-			// number of subscriptions
-			r2 := r.At(2)
-			if r2.Type() != ReplyInteger {
-				goto Invalid
-			}
-
-			m.Subscriptions = r2.Int()
-		case m.Type == MessagePSubscribe || m.Type == MessagePUnsubscribe:
-			m.Pattern = r1.Str()
-
-			// number of subscriptions
-			r2 := r.At(2)
-			if r2.Type() != ReplyInteger {
-				goto Invalid
-			}
-
-			m.Subscriptions = r2.Int()
-		case m.Type == MessageMessage:
-			m.Channel = r1.Str()
-
-			// payload
-			r2 := r.At(2)
-			if r2.Type() != ReplyString {
-				goto Invalid
-			}
-
-			m.Payload = r2.Str()
-		case m.Type == MessagePMessage:
-			m.Pattern = r1.Str()
-
-			// name of the originating channel
-			r2 := r.At(2)
-			if r2.Type() != ReplyString {
-				goto Invalid
-			}
-
-			m.Channel = r2.Str()
-
-			// payload
-			r3 := r.At(3)
-			if r3.Type() != ReplyString {
-				goto Invalid
-			}
-
-			m.Payload = r3.Str()
-		default:
-			goto Invalid
-		}
-
-		c.messageChan <- m
 		return
-
-	Invalid:
-		// Invalid reply
-		c.messageChan <- &Message{Type: MessageError, Error: newError("received invalid pub/sub reply",
-			ErrorInvalidReply)}
 	}
+
+	var r0, r1 *Reply
+	m := &Message{}
+
+	if r.Type() != ReplyMulti || r.Len() < 3 {
+		goto Invalid
+	}
+
+	r0 = r.At(0)
+	if r0.Type() != ReplyString {
+		goto Invalid
+	}
+
+	// first argument is the message type
+	switch r0.Str() {
+	case "subscribe":
+		m.Type = MessageSubscribe
+	case "unsubscribe":
+		m.Type = MessageUnsubscribe
+	case "psubscribe":
+		m.Type = MessagePSubscribe
+	case "punsubscribe":
+		m.Type = MessagePUnsubscribe
+	case "message":
+		m.Type = MessageMessage
+	case "pmessage":
+		m.Type = MessagePMessage
+	default:
+		goto Invalid
+	}
+
+	// second argument
+	r1 = r.At(1)
+	if r1.Type() != ReplyString {
+		goto Invalid
+	}
+
+	switch {
+	case m.Type == MessageSubscribe || m.Type == MessageUnsubscribe:
+		m.Channel = r1.Str()
+
+		// number of subscriptions
+		r2 := r.At(2)
+		if r2.Type() != ReplyInteger {
+			goto Invalid
+		}
+
+		m.Subscriptions = r2.Int()
+	case m.Type == MessagePSubscribe || m.Type == MessagePUnsubscribe:
+		m.Pattern = r1.Str()
+
+		// number of subscriptions
+		r2 := r.At(2)
+		if r2.Type() != ReplyInteger {
+			goto Invalid
+		}
+
+		m.Subscriptions = r2.Int()
+	case m.Type == MessageMessage:
+		m.Channel = r1.Str()
+
+		// payload
+		r2 := r.At(2)
+		if r2.Type() != ReplyString {
+			goto Invalid
+		}
+
+		m.Payload = r2.Str()
+	case m.Type == MessagePMessage:
+		m.Pattern = r1.Str()
+
+		// name of the originating channel
+		r2 := r.At(2)
+		if r2.Type() != ReplyString {
+			goto Invalid
+		}
+
+		m.Channel = r2.Str()
+
+		// payload
+		r3 := r.At(3)
+		if r3.Type() != ReplyString {
+			goto Invalid
+		}
+
+		m.Payload = r3.Str()
+	default:
+		goto Invalid
+	}
+
+	c.messageChan <- m
+	return
+
+Invalid:
+	// Invalid reply
+	c.messageChan <- &Message{Type: MessageError, Error: newError("received invalid pub/sub reply",
+		ErrorInvalidReply)}
 }
 
 func (c *connection) handleSubscription(es *envSubscription) {
