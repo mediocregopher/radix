f01aef90f738d78757d8cf4719461d39e738a127
fzzbt <juhanipm@gmail.com>
2012-05-19 16:42:32 +0300

    docstring update

--- redis/doc.go
+++ redis/doc.go
@@ -1,8 +1,6 @@
 // This package implements an asynchronous Redis client.
 //
 // Client is a structure for accessing a Redis database. 
-// After establishing a connection with NewClient, commands can be executed with Client.Command.
-// Client.Command returns a Reply with different methods for accessing the retrieved values.
 // Client.MultiCommand can be used for sending multiple commands in a single request and
 // Client.Transaction offers a simple way for executing atomic requests.
 // Client.Subscription returns a Subscription that can be used for listening published messages.
