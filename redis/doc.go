2e200ecb49b6a1ab4cd8aa5735eae49a86467d04
fzzbt <juhanipm@gmail.com>
2012-05-19 12:20:32 +0300

    removed extra space.

--- redis/doc.go
+++ redis/doc.go
@@ -1,7 +1,7 @@
 // This package implements an asynchronous Redis client.
 //
 // Client is a structure for accessing a Redis database. 
-// After establishing a connection with NewClient, commands can be executed with Client.Command. 
+// After establishing a connection with NewClient, commands can be executed with Client.Command.
 // Client.Command returns a Reply with different methods for accessing the retrieved values.
 // Client.MultiCommand can be used for sending multiple commands in a single request and
 // Client.Transaction offers a simple way for executing atomic requests.
