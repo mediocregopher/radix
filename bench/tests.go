19b5902ec0fac12641eea923629502812dc19d55
fzzbt <juhanipm@gmail.com>
2012-05-25 19:33:42 +0300

    bench: fix get bench to actually call get

--- bench/tests.go
+++ bench/tests.go
@@ -33,6 +33,6 @@ func setHandle(data string, c *redis.Client, ch chan struct{}) {
 
 func getHandle(data string, c *redis.Client, ch chan struct{}) {
     for _ = range ch {
-        c.Set(key, data)
+        c.Get(key)
     }
 }
