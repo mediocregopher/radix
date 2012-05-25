7ef55d7cd47fa0cf38aa1ce740401ab8523373f0
fzzbt <juhanipm@gmail.com>
2012-05-25 16:45:14 +0300

    bench: added ping

--- bench/tests.go
+++ bench/tests.go
@@ -8,8 +8,15 @@ var key string = "foo:rand:000000000000"
 var tests = make(map[string]func(string, *redis.Client, chan struct{}))
 
 func init() {
+    tests["ping"] = pingHandle
     tests["set"] = setHandle
 	tests["get"] = getHandle
+}
+
+func pingHandle(data string, c *redis.Client, ch chan struct{}) {
+    for _ = range ch {
+        c.Ping()
+    }
 }
 
 func setHandle(data string, c *redis.Client, ch chan struct{}) {
