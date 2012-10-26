72bc774da73761230b4f6d0995023b2f0a03f469
fzzbt <juhanipm@gmail.com>
2012-10-26 20:15:46 +0300

    refactoring in connpool.

--- redis/connpool.go
+++ redis/connpool.go
@@ -7,9 +7,9 @@ import (
 
 // connPool is a stack-like structure that holds the connections of a Client.
 type connPool struct {
+	sync.Mutex
 	available int
 	free      list.List
-	lock      sync.Mutex
 	emptyCond *sync.Cond
 	config    *Config
 	closed    bool
@@ -20,13 +20,13 @@ func newConnPool(config *Config) *connPool {
 	cp.available = config.PoolCapacity
 	cp.config = config
 	cp.closed = false
-	cp.emptyCond = sync.NewCond(&cp.lock)
+	cp.emptyCond = sync.NewCond(cp)
 	return cp
 }
 
 func (cp *connPool) push(c *conn) {
-	cp.lock.Lock()
-	defer cp.lock.Unlock()
+	cp.Lock()
+	defer cp.Unlock()
 	if cp.closed {
 		return
 	}
@@ -40,8 +40,8 @@ func (cp *connPool) push(c *conn) {
 }
 
 func (cp *connPool) pull() (c *conn, err *Error) {
-	cp.lock.Lock()
-	defer cp.lock.Unlock()
+	cp.Lock()
+	defer cp.Unlock()
 	if cp.closed {
 		return nil, newError("connection pool is closed", ErrorConnection)
 	}
@@ -67,8 +67,8 @@ func (cp *connPool) pull() (c *conn, err *Error) {
 }
 
 func (cp *connPool) close() {
-	cp.lock.Lock()
-	defer cp.lock.Unlock()
+	cp.Lock()
+	defer cp.Unlock()
 	for e := cp.free.Front(); e != nil; e = e.Next() {
 		c, _ := e.Value.(*conn)
 		c.close()
