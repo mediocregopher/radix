0187b53d3b23bc46b4d8c980a3b0aef72e2efdc5
Geoff Hayes <hayesgm@gmail.com>
2014-09-22 00:03:53 -0700

    Merge similar code patterns and add a test case for PSubscribe

--- extra/pubsub/sub.go
+++ extra/pubsub/sub.go
@@ -146,32 +146,24 @@ func (c *SubClient) parseReply(reply *redis.Reply) *SubReply {
 		} else {
 			sr.SubCount = count
 		}
-	case "message":
+	case "message", "pmessage":
+		var chanI, msgI int
+
+		if rtype == "message" {
+			chanI, msgI = 1, 2
+		} else { // "pmessage"
+			chanI, msgI = 2, 3
+		}
+
 		sr.Type = MessageReply
-		channel, err := reply.Elems[1].Str()
+		channel, err := reply.Elems[chanI].Str()
 		if err != nil {
 			sr.Err = errors.New("subscription multireply does not have string value for channel")
 			sr.Type = ErrorReply
 			return sr
 		}
 		sr.Channel = channel
-		msg, err := reply.Elems[2].Str()
-		if err != nil {
-			sr.Err = errors.New("message reply does not have string value for body")
-			sr.Type = ErrorReply
-		} else {
-			sr.Message = msg
-		}
-	case "pmessage":
-		sr.Type = MessageReply
-		channel, err := reply.Elems[2].Str()
-		if err != nil {
-			sr.Err = errors.New("subscription multireply does not have string value for channel")
-			sr.Type = ErrorReply
-			return sr
-		}
-		sr.Channel = channel
-		msg, err := reply.Elems[3].Str()
+		msg, err := reply.Elems[msgI].Str()
 		if err != nil {
 			sr.Err = errors.New("message reply does not have string value for body")
 			sr.Type = ErrorReply
--- extra/pubsub/sub_test.go
+++ extra/pubsub/sub_test.go
@@ -77,3 +77,73 @@ func TestSubscribe(t *testing.T) {
 		t.Fatal(fmt.Sprintf("Unexpected subscription count, Expected: 0, Found: %d", sr.SubCount))
 	}
 }
+
+func TestPSubscribe(t *testing.T) {
+	pub, err := redis.DialTimeout("tcp", "localhost:6379", time.Duration(10)*time.Second)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	client, err := redis.DialTimeout("tcp", "localhost:6379", time.Duration(10)*time.Second)
+	if err != nil {
+		t.Fatal(err)
+	}
+	sub := NewSubClient(client)
+
+	pattern := "patternThen*"
+	message := "Hello, World!"
+
+	sr := sub.PSubscribe(pattern)
+	if sr.Err != nil {
+		t.Fatal(sr.Err)
+	}
+
+	if sr.Type != SubscribeReply {
+		t.Fatal("Did not receive a subscribe reply")
+	}
+
+	if sr.SubCount != 1 {
+		t.Fatal(fmt.Sprintf("Unexpected subscription count, Expected: 0, Found: %d", sr.SubCount))
+	}
+
+	r := pub.Cmd("PUBLISH", "patternThenHello", message)
+	if r.Err != nil {
+		t.Fatal(r.Err)
+	}
+
+	subChan := make(chan *SubReply)
+	go func() {
+		subChan <- sub.Receive()
+	}()
+
+	select {
+	case sr = <-subChan:
+	case <-time.After(time.Duration(10) * time.Second):
+		t.Fatal("Took too long to Receive message")
+	}
+
+	if sr.Err != nil {
+		t.Fatal(sr.Err)
+	}
+
+	if sr.Type != MessageReply {
+		t.Fatal("Did not receive a message reply")
+	}
+
+	if sr.Message != message {
+		t.Fatal(fmt.Sprintf("Did not recieve expected message '%s', instead got: '%s'", message, sr.Message))
+	}
+
+	sr = sub.PUnsubscribe(pattern)
+	if sr.Err != nil {
+		t.Fatal(sr.Err)
+	}
+
+	if sr.Type != UnsubscribeReply {
+		t.Fatal("Did not receive a unsubscribe reply")
+	}
+
+	if sr.SubCount != 0 {
+		t.Fatal(fmt.Sprintf("Unexpected subscription count, Expected: 0, Found: %d", sr.SubCount))
+	}
+}
