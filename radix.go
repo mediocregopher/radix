63955b601e070cb6591ba91703edc60cf13db7d9
Brian Picciano <mediocregopher@gmail.com>
2014-11-22 16:08:23 -0500

    add a radix.go file so go get will be quiet

---
+++ radix.go
@@ -0,0 +1,6 @@
+// Radix is a simple redis driver. This top-level package is a wrapper for its
+// two sub-packages: redis, which contains the single-connection redis client
+// (the most commonly used part of this package) and extra, which contains other
+// sub-packages for doing things like pooling connections and interacting with a
+// redis cluster.
+package radix
