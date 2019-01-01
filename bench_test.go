6d46457152bd8256116ecf9757ddad01463f8835
Justin Nuß <nuss.justin@gmail.com>
2019-01-01 20:08:11 +0100

    Add redispipe to benchmarks

--- bench_test.go
+++ bench_test.go
@@ -1,8 +1,13 @@
 package radix
 
 import (
+	"context"
 	"runtime"
 	. "testing"
+	"time"
+
+	"github.com/joomcode/redispipe/redis"
+	"github.com/joomcode/redispipe/redisconn"
 
 	redigo "github.com/gomodule/redigo/redis"
 )
@@ -16,12 +21,13 @@ func newRedigo() redigo.Conn {
 }
 
 func BenchmarkSerialGetSet(b *B) {
-	radix, err := Dial("tcp", "127.0.0.1:6379")
-	if err != nil {
-		b.Fatal(err)
-	}
-	defer radix.Close()
 	b.Run("radix", func(b *B) {
+		radix, err := Dial("tcp", "127.0.0.1:6379")
+		if err != nil {
+			b.Fatal(err)
+		}
+		defer radix.Close()
+		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
 			if err := radix.Do(Cmd(nil, "SET", "foo", "bar")); err != nil {
 				b.Fatal(err)
@@ -33,8 +39,9 @@ func BenchmarkSerialGetSet(b *B) {
 		}
 	})
 
-	red := newRedigo()
 	b.Run("redigo", func(b *B) {
+		red := newRedigo()
+		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
 			if _, err := red.Do("SET", "foo", "bar"); err != nil {
 				b.Fatal(err)
@@ -44,12 +51,55 @@ func BenchmarkSerialGetSet(b *B) {
 			}
 		}
 	})
+
+	b.Run("redispipe", func(b *B) {
+		pipe, err := redisconn.Connect(context.Background(), "127.0.0.1:6379", redisconn.Opts{
+			Logger:     redisconn.NoopLogger{},
+			WritePause: 150 * time.Microsecond,
+		})
+		defer pipe.Close()
+		if err != nil {
+			b.Fatal(err)
+		}
+		sync := redis.Sync{pipe}
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			if res := sync.Do("SET", "foo", "bar"); redis.AsError(res) != nil {
+				b.Fatal(res)
+			}
+			if res := sync.Do("GET", "foo"); redis.AsError(res) != nil {
+				b.Fatal(res)
+			}
+		}
+	})
+
+	b.Run("redispipe_pause0", func(b *B) {
+		pipe, err := redisconn.Connect(context.Background(), "127.0.0.1:6379", redisconn.Opts{
+			Logger:     redisconn.NoopLogger{},
+			WritePause: -1,
+		})
+		defer pipe.Close()
+		if err != nil {
+			b.Fatal(err)
+		}
+		sync := redis.Sync{pipe}
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			if res := sync.Do("SET", "foo", "bar"); redis.AsError(res) != nil {
+				b.Fatal(res)
+			}
+			if res := sync.Do("GET", "foo"); redis.AsError(res) != nil {
+				b.Fatal(res)
+			}
+		}
+	})
 }
 
 func BenchmarkParallelGetSet(b *B) {
 	parallel := runtime.GOMAXPROCS(0) * 8
 
 	do := func(b *B, fn func()) {
+		b.ResetTimer()
 		b.SetParallelism(parallel)
 		b.RunParallel(func(pb *PB) {
 			for pb.Next() {
@@ -58,50 +108,80 @@ func BenchmarkParallelGetSet(b *B) {
 		})
 	}
 
-	radix, err := NewPool("tcp", "127.0.0.1:6379", parallel, PoolPipelineConcurrency(parallel))
-	if err != nil {
-		b.Fatal(err)
-	}
-	defer radix.Close()
 	b.Run("radix", func(b *B) {
-		do(b, func() {
-			err := radix.Do(WithConn("foo", func(conn Conn) error {
-				if err := conn.Do(Cmd(nil, "SET", "foo", "bar")); err != nil {
-					return err
-				}
-				var out string
-				if err := conn.Do(Cmd(&out, "GET", "foo")); err != nil {
-					return err
-				} else if out != "bar" {
-					b.Fatal("got wrong value")
-				}
-				return nil
-			}))
+		b.Run("no pipelining", func(b *B) {
+			radix, err := NewPool("tcp", "127.0.0.1:6379", parallel, PoolPipelineWindow(0))
 			if err != nil {
 				b.Fatal(err)
 			}
+			defer radix.Close()
+
+			do(b, func() {
+				err := radix.Do(WithConn("foo", func(conn Conn) error {
+					if err := conn.Do(Cmd(nil, "SET", "foo", "bar")); err != nil {
+						return err
+					}
+					var out string
+					if err := conn.Do(Cmd(&out, "GET", "foo")); err != nil {
+						return err
+					} else if out != "bar" {
+						b.Fatal("got wrong value")
+					}
+					return nil
+				}))
+				if err != nil {
+					b.Fatal(err)
+				}
+			})
+		})
+
+		b.Run("one pipeline", func(b *B) {
+			radix, err := NewPool("tcp", "127.0.0.1:6379", parallel, PoolPipelineConcurrency(1))
+			if err != nil {
+				b.Fatal(err)
+			}
+			defer radix.Close()
+
+			do(b, func() {
+				if err := radix.Do(Cmd(nil, "SET", "foo", "bar")); err != nil {
+					b.Fatal(err)
+				}
+				var out string
+				if err := radix.Do(Cmd(&out, "GET", "foo")); err != nil {
+					b.Fatal(err)
+				} else if out != "bar" {
+					b.Fatal("got wrong value")
+				}
+			})
+		})
+
+		b.Run("multiple pipelines", func(b *B) {
+			radix, err := NewPool("tcp", "127.0.0.1:6379", parallel, PoolPipelineConcurrency(parallel))
+			if err != nil {
+				b.Fatal(err)
+			}
+			defer radix.Close()
+
+			do(b, func() {
+				if err := radix.Do(Cmd(nil, "SET", "foo", "bar")); err != nil {
+					b.Fatal(err)
+				}
+				var out string
+				if err := radix.Do(Cmd(&out, "GET", "foo")); err != nil {
+					b.Fatal(err)
+				} else if out != "bar" {
+					b.Fatal("got wrong value")
+				}
+			})
 		})
 	})
 
-	b.Run("radix_(pipelined)", func(b *B) {
-		do(b, func() {
-			if err := radix.Do(Cmd(nil, "SET", "foo", "bar")); err != nil {
-				b.Fatal(err)
-			}
-			var out string
-			if err := radix.Do(Cmd(&out, "GET", "foo")); err != nil {
-				b.Fatal(err)
-			} else if out != "bar" {
-				b.Fatal("got wrong value")
-			}
-		})
-	})
-
-	red := &redigo.Pool{MaxIdle: parallel, Dial: func() (redigo.Conn, error) {
-		return newRedigo(), nil
-	}}
-	defer red.Close()
 	b.Run("redigo", func(b *B) {
+		red := &redigo.Pool{MaxIdle: parallel, Dial: func() (redigo.Conn, error) {
+			return newRedigo(), nil
+		}}
+		defer red.Close()
+
 		do(b, func() {
 			conn := red.Get()
 			defer conn.Close()
@@ -112,6 +192,27 @@ func BenchmarkParallelGetSet(b *B) {
 				b.Fatal(err)
 			} else if out != "bar" {
 				b.Fatal("got wrong value")
+			}
+		})
+	})
+
+	b.Run("redispipe", func(b *B) {
+		pipe, err := redisconn.Connect(context.Background(), "127.0.0.1:6379", redisconn.Opts{
+			Logger:     redisconn.NoopLogger{},
+			WritePause: 150 * time.Microsecond,
+		})
+		if err != nil {
+			b.Fatal(err)
+		}
+		defer pipe.Close()
+		sync := redis.Sync{pipe}
+
+		do(b, func() {
+			if res := sync.Do("SET", "foo", "bar"); redis.AsError(res) != nil {
+				b.Fatal(res)
+			}
+			if res := sync.Do("GET", "foo"); redis.AsError(res) != nil {
+				b.Fatal(err)
 			}
 		})
 	})
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/mediocregopher/radix/v3
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gomodule/redigo v2.0.0+incompatible
+	github.com/joomcode/errorx v0.1.0 // indirect
+	github.com/joomcode/redispipe v0.9.0 // indirect
 	github.com/mediocregopher/mediocre-go-lib v0.0.0-20181029021733-cb65787f37ed
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.2.2
