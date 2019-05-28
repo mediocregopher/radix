a4b003e8968c1faef8f031557bcb25a7cdd70afd
Brian Picciano <mediocregopher@gmail.com>
2019-05-28 19:47:32 -0400

    flesh out trace's package documentation a bit

--- trace/trace.go
+++ trace/trace.go
@@ -1,4 +1,13 @@
-// Disclaimer: Note that Package trace is not stable yet and is subject to change.
+// Package trace contains all the types provided for tracing within the radix
+// package. With tracing a user is able to pull out fine-grained runtime events
+// as they happen, which is useful for gathering metrics, logging, performance
+// analysis, etc...
+//
+// BIG LOUD DISCLAIMER DO NOT IGNORE THIS: while the main radix package is
+// stable and will always remain backwards compatible, trace is still under
+// active development and may undergo changes to its types and other features.
+// The methods in the main radix package which invoke trace types are guaranteed
+// to remain stable.
 package trace
 
 import (
