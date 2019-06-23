e1f973fd2c1f7045ff3d477027b69adede3051e3
Jack Lindamood <jlindamo@justin.tv>
2019-06-23 11:45:49 -0700

    Add aliases

--- alias.go
+++ alias.go
@@ -2,5 +2,98 @@ package radix
 
 import v3 "github.com/mediocregopher/radix/v3"
 
+// action.go
 type Action = v3.Action
 type CmdAction = v3.CmdAction
+var FlatCmd = v3.FlatCmd
+type MaybeNil = v3.MaybeNil
+type EvalScript = v3.EvalScript
+var NewEvalScript = v3.NewEvalScript
+var Pipeline = v3.Pipeline
+var WithConn = v3.WithConn
+
+// cluster.go
+type ClusterCanRetryAction = v3.ClusterCanRetryAction
+var ClusterPoolFunc = v3.ClusterPoolFunc
+var ClusterSyncEvery = v3.ClusterSyncEvery
+var ClusterWithTrace = v3.ClusterWithTrace
+type Cluster = v3.Cluster
+var NewCluster = v3.NewCluster
+
+// cluster_crc16.go
+var CRC16 = v3.CRC16
+var ClusterSlot = v3.ClusterSlot
+
+
+// cluster_topo.go
+type ClusterNode = v3.ClusterNode
+type ClusterTopo = v3.ClusterTopo
+
+// pool.go
+var ErrPoolEmpty = v3.ErrPoolEmpty
+type PoolOpt = v3.PoolOpt
+var PoolConnFunc = v3.PoolConnFunc
+var PoolPingInterval = v3.PoolPingInterval
+var PoolRefillInterval = v3.PoolRefillInterval
+var PoolOnEmptyWait = v3.PoolOnEmptyWait
+var PoolOnEmptyCreateAfter = v3.PoolOnEmptyCreateAfter
+var PoolOnEmptyErrAfter = v3.PoolOnEmptyErrAfter
+var PoolOnFullClose = v3.PoolOnFullClose
+var PoolOnFullBuffer = v3.PoolOnFullBuffer
+var PoolPipelineConcurrency = v3.PoolPipelineConcurrency
+var PoolPipelineWindow = v3.PoolPipelineWindow
+var PoolWithTrace = v3.PoolWithTrace
+type Pool = v3.Pool
+var NewPool = v3.NewPool
+
+// pubsub.go
+type PubSubMessage = v3.PubSubMessage
+type PubSubConn = v3.PubSubConn
+var PubSub = v3.PubSub
+
+// pubsub_persistent.go
+var PersistentPubSub = v3.PersistentPubSub
+
+// pubsub_stub.go
+var PubSubStub = v3.PubSubStub
+
+// radix.go
+type Client = v3.Client
+type ClientFunc = v3.ClientFunc
+var DefaultClientFunc = v3.DefaultClientFunc
+type Conn = v3.Conn
+var NewConn = v3.NewConn
+type ConnFunc = v3.ConnFunc
+var DefaultConnFunc = v3.DefaultConnFunc
+type DialOpt = v3.DialOpt
+var DialConnectTimeout = v3.DialConnectTimeout
+var DialReadTimeout = v3.DialReadTimeout
+var DialWriteTimeout = v3.DialWriteTimeout
+var DialTimeout = v3.DialTimeout
+var DialAuthPass = v3.DialAuthPass
+var DialSelectDB = v3.DialSelectDB
+var DialUseTLS = v3.DialUseTLS
+var Dial = v3.Dial
+
+// scanner.go
+type Scanner = v3.Scanner
+type ScanOpts = v3.ScanOpts
+var ScanAllKeys = v3.ScanAllKeys
+var NewScanner = v3.NewScanner
+
+// sentinel.go
+type SentinelOpt = v3.SentinelOpt
+var SentinelConnFunc = v3.SentinelConnFunc
+var SentinelPoolFunc = v3.SentinelPoolFunc
+type Sentinel = v3.Sentinel
+var NewSentinel = v3.NewSentinel
+
+// stream.go
+type StreamEntryID = v3.StreamEntryID
+type StreamEntry = v3.StreamEntry
+type StreamReaderOpts = v3.StreamReaderOpts
+type StreamReader = v3.StreamReader
+var NewStreamReader = v3.NewStreamReader
+
+// stub.go
+var Stub = v3.Stub
