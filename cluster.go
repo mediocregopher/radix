954cd11620183f3c540e86a46de1ff1feede7894
zachary.du <zachary.du@bybit.com>
2022-05-30 16:56:20 +0800

    add a method getting the masterNode without lock to fix the deadlock for DoSecondary

--- cluster.go
+++ cluster.go
@@ -508,7 +508,7 @@ func (c *Cluster) addrForKey(key string) string {
 	return ""
 }
 
-// add the getting master node without lock to fix the fix deadlock
+// v3.8.5 add the getting master node without lock to fix the fix deadlock
 func (c *Cluster) addrForKeyWithNoLock(key string) string {
 	s := ClusterSlot([]byte(key))
 	for _, t := range c.primTopo {
